264ebaa63da88549cca20b8507a36c2ec33aed6e
love-sora-chan <96691095+love-sora-chan@users.noreply.github.com>
2024-04-20 14:52:05 +0800

    Update RFID.h

--- arduino/midterm_project/RFID.h
+++ arduino/midterm_project/RFID.h
@@ -21,19 +21,63 @@ class RFID{
       return res;
     }
 
-    String get_UID(){
+    // String get_UID(){
       
-      if( mfrc522->PICC_IsNewCardPresent() && mfrc522->PICC_ReadCardSerial() ){
+    //   if( mfrc522->PICC_IsNewCardPresent() && mfrc522->PICC_ReadCardSerial() ){
         
-        String UID = dump_byte_array(mfrc522->uid.uidByte, mfrc522->uid.size);
-        Serial.println(UID);
+    //     String UID = dump_byte_array(mfrc522->uid.uidByte, mfrc522->uid.size);
+    //     Serial.println(UID);
+    //     mfrc522->PICC_HaltA();
+    //     mfrc522->PCD_StopCrypto1(); 
+
+    //     return UID;
+    //   }
+    //   else{
+    //     return "NONE";
+    //   }
+    // }
+
+
+    // get the UID of RFID card in unsigned long format
+    unsigned long get_UID(){ 
+      if( mfrc522->PICC_IsNewCardPresent() && mfrc522->PICC_ReadCardSerial() ){
+        unsigned long UID = 0;
+        for(int i=0; i<4; i++){
+          UID += mfrc522->uid.uidByte[i];
+          UID <<= 8*(i<3);
+        }
         mfrc522->PICC_HaltA();
         mfrc522->PCD_StopCrypto1(); 
-
         return UID;
       }
       else{
-        return "NONE";
+        return 0;
       }
+    }
+
+    // turn the UID of RFID card from unsigned long into char* format, returns 1 for success, 0 for fail
+    // bool UID_to_string(unsigned long UID,char* UID_in_string){
+    //   if(UID!=0){
+    //     char upper_UID_string[6];
+    //     char lower_UID_string[6];
+    //     // char UID_in_string[15];
+    //     int upper_UID = UID>>16;
+    //     int lower_UID = (UID<<16)>>16;
+    //     snprintf(upper_UID_string, 6, "%X", upper_UID);
+    //     snprintf(lower_UID_string, 6, "%X", lower_UID);
+    //     strcpy(UID_in_string,upper_UID_string);
+    //     strcat(UID_in_string,lower_UID_string);
+    //     return true;
+    //   }
+    //   else{
+    //     return false;
+    //   }
+    // }
+
+    void UID_to_byteArray(byte* byteArray, unsigned long UID){
+      byteArray[0] = (byte)((UID >> 24) & 0xFF);
+      byteArray[1] = (byte)((UID >> 16) & 0xFF);
+      byteArray[2] = (byte)((UID >> 8) & 0xFF);
+      byteArray[3] = (byte)(UID & 0xFF);
     }
 };
